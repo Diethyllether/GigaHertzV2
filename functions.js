0b3665e73b6161184570e73a7a36eb01345c1bb1
Diethyllether <felipecornelio@outlook.com.br>
2025-08-19 15:58:03 -0300

    First Commit

---
+++ functions.js
@@ -0,0 +1,69 @@
+// Tema
+
+function changeColor() {
+  const root = document.documentElement;
+  const logo = document.getElementById("logo");
+  const btn = document.getElementById("ball");
+  switch (localStorage.getItem("theme")) {
+    case "dark":
+      // Light Mode
+      root.style.setProperty("--main-bg-color", "#E5E5E5");
+      root.style.setProperty("--secondary-color", "#3185FC");
+      root.style.setProperty("--text-color", "black");
+      root.style.setProperty("--btn-bg", "white");
+      logo.src = "assets/images/logo-alt.png";
+      btn.style.marginLeft = "0px";
+      localStorage.setItem("theme", "light");
+      break;
+
+    case "light":
+      // Dark Mode
+      root.style.setProperty("--main-bg-color", "#1E1E1E");
+      root.style.setProperty("--secondary-color", "#FF3D10");
+      root.style.setProperty("--text-color", "white");
+      root.style.setProperty("--btn-bg", "green");
+      logo.src = "assets/images/logo.png";
+      btn.style.marginLeft = "15px";
+      localStorage.setItem("theme", "dark");
+      break;
+  }
+}
+
+// Menu
+let x = 0;
+function showMenu() {
+  const dropdownMenu = document.getElementById("dropdown");
+  switch (x) {
+    case 0:
+      dropdownMenu.style.top = "-500px";
+      x = 1;
+      break;
+    case 1:
+      dropdownMenu.style.top = "80px";
+      x = 0;
+      break;
+  }
+}
+
+// Pro carrinho
+
+function mudarPreco(opcao) {
+  const inputPreco = document.getElementById("number");
+  let y = parseInt(inputPreco.value);
+  switch (opcao) {
+    case "sobe":
+      y++;
+      inputPreco.value = y;
+      break;
+
+    case "desce":
+      if (y > 0) {
+        y--;
+        inputPreco.value = y;
+      }
+      break;
+
+    default:
+      break;
+  }
+}
